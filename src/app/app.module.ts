24e4af308f70f0832a3973135c3e82da48ed07f8
aleksa.zubic <aleksa.zubic@xcentric.it>
2023-05-05 12:57:01 +0200

    RAL-4 Login and Sign Up pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit{
+  form!: FormGroup;
+  isLoginPage = true;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {
+  }
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: '',
+      password: '',
+      email: '',
+      gender: '',
+      name: '',
+      surname: ''
+    })
+  }
+
+  submit(){
+    if(this.isLoginPage) {
+      this.http.post('http://localhost:8080/api/auth/login', this.form.getRawValue())
+        .subscribe((res:any) => {
+          console.log(res);
+        })
+    }
+    else
+    {
+      this.http.post('http://localhost:8080/api/auth/register', this.form.getRawValue())
+        .subscribe((res:any) => {
+          console.log(res);
+        })
+    }
+  }
+}
