888fcfc59f4e519df39eb8a708f23d6e7642853d
aleksa.zubic <aleksa.zubic@xcentric.it>
2023-05-10 11:37:34 +0200

    RAL-5 Home page and managers page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ManagersComponent } from './managers/managers.component';
 
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'managers', component: ManagersComponent},
+  {path: 'login', component: AuthComponent},
+  {path: 'register', component: AuthComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ManagersComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
